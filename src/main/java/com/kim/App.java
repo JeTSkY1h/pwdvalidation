5e07d2a0d94210a3abb67d0e0956363b58027b35
JeTSkY1h <jetsky1h@googlemail.com>
2022-05-20 11:10:24 +0200

    Initial Commit

---
+++ src/main/java/com/kim/App.java
@@ -0,0 +1,25 @@
+package com.kim;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    
+    public static boolean pwdValid(String pwd) {
+        String regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pwd);
+
+        return matcher.find();
+    }
+    
+}
---
+++ src/test/java/com/kim/AppTest.java
@@ -0,0 +1,55 @@
+package com.kim;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithFalseBecausePwdisLongerThen8ButhasNoNumber(){
+        //given
+        String pwd = "ichbinlangGenug";
+        //when
+        boolean res = App.pwdValid(pwd);
+        //then
+        assertFalse(res);
+    }
+
+    @Test 
+    void shouldBeFalseBecausePwdHasNoSpecialChrakter() {
+        //given
+        String pwd = "ichbinlanASdggenug8";
+        //when
+        boolean res = App.pwdValid(pwd);
+        //then
+        assertFalse(res);
+    }
+
+    @Test
+    void shouldBeFalseBecausePwdHasNoUpperCAseLetter() {
+        //given
+        String pwd = "asdasda!sdasdasdadsfe8";
+        //when
+        boolean res = App.pwdValid(pwd);
+        //then
+        assertFalse(res);
+    }
+
+    @Test
+    void shouldBeTrueBecausePwdIsLongEnoughHasUpperCaseLetterNumberAndSpecialCharakter(){
+        //given
+        String pwd = "BeiMirIstAllesRichtig!!!111elf";
+        //when
+        boolean res = App.pwdValid(pwd);
+        //then
+        assertTrue(res);
+    }
+
+
+}
